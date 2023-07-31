5e172440169622bd73cf6ea2d3dbfd78797bb7ab
Rohan Brahmakshatriya <rohan.kshatriya92@gmail.com>
2023-07-31 19:49:37 +0530

    prepare base model completed

--- src/cnnClassifiers/config/configuration.py
+++ src/cnnClassifiers/config/configuration.py
@@ -1,6 +1,7 @@
 from cnnClassifiers.constants import *
 from cnnClassifiers.utils.common import read_yaml,create_dictionaries
-from cnnClassifiers.entity.config_entity import DataIngestionConfig
+from cnnClassifiers.entity.config_entity import (DataIngestionConfig,PrepareBaseModelConfig)
+
 
 class configurationManager:
     def __init__(self,
@@ -26,4 +27,28 @@ class configurationManager:
         )
         
         return data_ingestion_config
+    
+    
+    
+    
+    
+    def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
+        config = self.config.prepare_base_model
+        
+        create_dictionaries([config.root_dir])
+
+        prepare_base_model_config = PrepareBaseModelConfig(
+            root_dir=Path(config.root_dir),
+            base_model_path=Path(config.base_model_path),
+            updated_base_model_path=Path(config.updated_base_model_path),
+            params_image_size=self.params.IMAGE_SIZE,
+            params_learning_rate=self.params.LEARNING_RATE,
+            params_include_top=self.params.INCLUDE_TOP,
+            params_weights=self.params.WEIGHTS,
+            params_classes=self.params.CLASSES
+        )
+
+        return prepare_base_model_config
+    
+    
     
